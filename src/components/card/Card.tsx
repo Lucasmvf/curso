27670a9facf6fbb31515d62d66f50e4f55b0e2e6
Lucas Martins <lucas_janoario@hotmail.com>
2025-05-07 13:48:04 -0300

    feat: improve card and section components

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,32 +1,31 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface ICardProps {
+
+export interface ICardProps {
+  href: string;
+  image: string;
   title: string;
   description: string;
-  image: string;
-  href: string;
 }
-
 export const Card = ({ title, description, image, href }: ICardProps) => {
-
   return (
     <Link href={href} className='hover:no-underline'>
       <article className='flex gap-2 flex-col p-2 rounded sm:hover:bg-primary'>
         <Image
-          height={640}
-          width={480}
-          draggable={false}
-          alt={title}
-          className='aspect-video object-cover rounded-2xl'
+          height={0}
           src={image}
+          alt={title}
+          width={1000}
+          draggable={false}
+          className='aspect-video object-cover rounded-2xl'
         />
 
         <h4 className='font-extrabold text-lg'>
           {title}
         </h4>
 
-        <p className='line-clamp-3'>
+        <p className='line-clamp-3 sm:line-clamp-4 md:line-clamp-5'>
           {description}
         </p>
       </article>
