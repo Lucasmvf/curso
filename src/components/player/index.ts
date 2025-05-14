66a2005eb549498aaec60d0256cc45f59c87dc03
Lucas Martins <lucas_janoario@hotmail.com>
2025-05-14 10:15:47 -0300

    feat: react player component

--- src/components/player/index.ts
+++ src/components/player/index.ts
@@ -1,4 +1,5 @@
 export * from './playlist/components/PlayerClassGroup';
-export * from './PlayerHeader';
-export * from './playlist/PlayerPlaylist';
 export * from './playlist/components/PlayerClass';
+export * from './playlist/PlayerPlaylist';
+export * from './PlayerVideoPlayer';
+export * from './PlayerHeader';
