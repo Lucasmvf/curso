fa08b9185afb9d7b542e14ac78681c11c9012d13
Lucas Martins <lucas_janoario@hotmail.com>
2025-05-17 15:08:45 -0300

    feat: add tabs components

--- src/components/player/player-class-details/PlayerClassDetails.tsx
+++ src/components/player/player-class-details/PlayerClassDetails.tsx
@@ -1,17 +1,24 @@
 'use client';
+import { useMemo } from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
 import { useRouter } from 'next/navigation';
 
 import { IPlayerClassGroupProps } from '../playlist/components/PlayerClassGroup';
+import { CourseHeader } from '@/components/course-header/CourseHeader';
 import { PlayerVideoPlayer } from './components/PlayerVideoPlayer';
-import { useMemo } from 'react';
 
 
 interface IPlayerClassDetailsProps {
+  course: {
+    title: string;
+    description: string;
+    numberOfClasses: number;
+  };
   playingClassId: string;
   playingCourseId: string;
   classGroups: Pick<IPlayerClassGroupProps, 'classes' | 'title'>[];
 }
-export const PlayerClassDetails = ({ playingCourseId, playingClassId, classGroups }: IPlayerClassDetailsProps) => {
+export const PlayerClassDetails = ({ playingCourseId, playingClassId, classGroups, course }: IPlayerClassDetailsProps) => {
   const router = useRouter();
 
 
@@ -31,7 +38,7 @@ export const PlayerClassDetails = ({ playingCourseId, playingClassId, classGroup
 
 
   return (
-    <div className='flex-1'>
+    <div className='flex-1 overflow-auto pb-10'>
       <div className='aspect-video'>
         <PlayerVideoPlayer
           videoId='bP47qRVRqQs'
@@ -39,9 +46,44 @@ export const PlayerClassDetails = ({ playingCourseId, playingClassId, classGroup
         />
       </div>
 
-      <div>
-        Descrição
-      </div>
+      <Tabs.Root defaultValue='class-details'>
+        <Tabs.List className='flex gap-4'>
+          <Tabs.Trigger
+            value='class-details'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Visão geral
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value='class-comments'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Comentários
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value='course-details'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Visão geral do curso
+          </Tabs.Trigger>
+        </Tabs.List>
+
+        <hr className='border-paper' />
+
+        <Tabs.Content value='class-details'>
+          Detalhes da aula
+        </Tabs.Content>
+        <Tabs.Content value='class-comments'>
+          Comentários da aula
+        </Tabs.Content>
+        <Tabs.Content value='course-details'>
+          <CourseHeader
+            title={course.title}
+            description={course.description}
+            numberOfClasses={course.numberOfClasses}
+          />
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 };
